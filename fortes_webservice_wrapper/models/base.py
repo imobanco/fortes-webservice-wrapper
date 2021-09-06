f3377c0a842a4efe3901a64723d1e354ed6da219
ariane <arianesc@ufrn.edu.br>
2021-09-06 11:56:57 -0300

    altera models

--- fortes_webservice_wrapper/models/base.py
+++ fortes_webservice_wrapper/models/base.py
@@ -1,49 +1,88 @@
 from pydantic import BaseModel, constr
 from typing import Literal, Optional, Union
 
-class Client(BaseModel):
-    """Informação de Cliente"""
-    codigo: str
-    nome: str
-    nomeFantasia: str
-    cnpjcpf: str
-    cep: str
-    logradouro: str
-    bairro: str
-    numero:str
-    complemento: str
-    uf:str
-    cidade: str
-    ddd: str
-    fone:str
-    fax: str
-    identificador: str
-    celular:str
-    email: str
-    contato: str
-    site: str
-    rg:str
-    ie: str
-    grupo:str
-    agenteCobrador:str
-    receita:str
-    contaContabil: str
-    Representante:str
-    Vencimento: str
-    Etiqueta: str
-    GeraNFAuto: str
-    RetemINSS: str
-    AliquotaISS: str
-    IM: str
-    CampoExtra1: str
-    CampoExtra2:str
-    CampoExtra3: str
-    CampoExtra4: str
-    CampoExtra5: str
-    CampoExtra6: str
-    CampoExtra7: str
-    CampoExtra8: str
-    CampoExtra9: str
-    CampoExtra10: str
-    Exterior: str
-    Inseto_de_IE: str
+
+class TClient(BaseModel):
+
+    codigo: constr(min_length=0, max_length=6)
+    nome: constr(min_length=0, max_length=80)
+    nomeFantasia: Optional[str]
+    cnpjcpf:  constr(min_length=11, max_length=14)
+    cep: Optional[str]
+    logradouro: Optional[str]
+    bairro: Optional[str]
+    numero: Optional[str]
+    complemento: Optional[str]
+    uf: Optional[str]
+    cidade: Optional[str]
+    ddd: Optional[str]
+    fone: Optional[str]
+    fax: Optional[str]
+    identificador: Optional[str]
+    celular: Optional[str]
+    email: Optional[str]
+    contato: Optional[str]
+    site: Optional[str]
+    rg: Optional[str]
+    ie: Optional[str]
+    grupo: Optional[str]
+    agenteCobrador: Optional[str]
+    receita: Optional[str]
+    contaContabil: Optional[str]
+    Representante: Optional[str]
+    Vencimento: Optional[int]
+    Etiqueta: Optional[int]
+    GeraNFAuto: Optional[int]
+    RetemINSS: Optional[int]
+    AliquotaISS: Optional[str]
+    IM: Optional[str]
+    CampoExtra1: Optional[str]
+    CampoExtra2: Optional[str]
+    CampoExtra3: Optional[str]
+    CampoExtra4: Optional[str]
+    CampoExtra5: Optional[str]
+    CampoExtra6: Optional[str]
+    CampoExtra7: Optional[str]
+    CampoExtra8: Optional[str]
+    CampoExtra9: Optional[str]
+    CampoExtra10: Optional[str]
+    Exterior: Optional[int]
+    Inseto_de_IE: Optional[int]
+
+
+class TFornecedor(BaseModel):
+    Codigo: Optional[str]
+    Nome: str
+    CNPJCPF:  constr(min_length=14, max_length=14)
+    CEP: Optional[str]
+    Logradouro: Optional[str]
+    Bairro: Optional[str]
+    Numero: Optional[str]
+    Complemento: Optional[str]
+    UF: Optional[str]
+    Cidade: Optional[str]
+    DDD: Optional[str]
+    Fone: Optional[str]
+    Fax: Optional[str]
+    Email: Optional[str]
+    Contato: Optional[str]
+    Site: Optional[str]
+    RG: Optional[str]
+    IE: Optional[str]
+    Grupo: Optional[str]
+    Receita: Optional[str]
+    IM: Optional[str]
+    Referencia: Optional[str]
+    FinalidadeTED: Optional[str]
+    FinalidadeDOC: Optional[str]
+    Celular: Optional[str]
+    Leiaute: Optional[str]
+    Banco: Optional[str]
+    BancoNumero: Optional[str]
+    Agencia: Optional[str]
+    CC: Optional[str]
+    DVAg: Optional[str]
+    DVCC: Optional[str]
+    ServicoPublico: Optional[int]
+    ICMS: Optional[int]
+    IPI: Optional[int]
