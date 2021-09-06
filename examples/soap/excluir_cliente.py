b9bac9c84cc83bce920ff1fd95a981b8504d4276
ariane <arianesc@ufrn.edu.br>
2021-09-06 11:56:41 -0300

    altera examples

---
+++ examples/soap/excluir_cliente.py
@@ -0,0 +1,6 @@
+from fortes_webservice_wrapper.wrapper.fortes_ws import FortesWrapper
+
+cpfcnpj = "57632889043"
+
+FortesWrapper().excluir_cliente(cpfcnpj)
+
---
+++ examples/soap/excluir_fornecedor.py
@@ -0,0 +1,42 @@
+from fortes_webservice_wrapper.wrapper.fortes_ws import FortesWrapper
+
+fornecedor = {
+                    "Codigo": "",
+                    "Nome": "Ana do Imobanco",
+                    "CNPJCPF": "31510515020",
+                    "CEP": "",
+                    "Logradouro": "",
+                    "Bairro": "",
+                    "Numero": "",
+                    "Complemento": "",
+                    "UF": "",
+                    "Cidade": "",
+                    "DDD": "",
+                    "Fone": "",
+                    "Fax": "",
+                    "Email": "",
+                    "Contato": "",
+                    "Site": "",
+                    "RG": "",
+                    "IE": "",
+                    "Grupo": "",
+                    "Receita": "",
+                    "IM": "",
+                    "Referencia": "",
+                    "FinalidadeTED": "",
+                    "FinalidadeDOC": "",
+                    "Celular": "",
+                    "Leiaute": "",
+                    "Banco": "",
+                    "BancoNumero": "",
+                    "Agencia": "",
+                    "CC": "",
+                    "DVAg": "",
+                    "DVCC": "",
+                    "ServicoPublico": "",
+                    "ICMS": "",
+                    "IPI": ""
+        }
+
+r = FortesWrapper().excluir_fornecedor_com_json(fornecedor)
+
