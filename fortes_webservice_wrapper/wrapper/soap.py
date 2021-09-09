d468ce3d467612ce4bb5f164556871870ddf0e46
Ariane Silveira <42891369+arianesc@users.noreply.github.com>
2021-09-09 11:05:01 -0300

    Update fortes_webservice_wrapper/wrapper/soap.py
    Co-authored-by: Rodrigo Castro <rodrigondec@gmail.com>

--- fortes_webservice_wrapper/wrapper/soap.py
+++ fortes_webservice_wrapper/wrapper/soap.py
@@ -4,8 +4,12 @@ from ..constants import WSDL_URL
 
 
 class BaseSoapWrapper:
-    def __init__(self):
-        self.wsdl = WSDL_URL
+    def __init__(self, wsdl_url=None):
+        if wsdl_url is None:
+            self.wsdl = WSDL_URL
+        else:
+            self.wsdl = wsdl_url
+        
 
     def get_client(self):
         return Client(self.wsdl)
